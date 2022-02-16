21874c47188a99715212b85555f454d65899bd77
DastanAlmazbek <dastanalmazbek1989@gmail.com>
2022-02-17 01:29:02 +0600

    swagger

--- spotifyapi/urls.py
+++ spotifyapi/urls.py
@@ -16,7 +16,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='spotifyapi',
+        default_version='v1',
+        description='Spotify'
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
+
+
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)), 
     path('admin/', admin.site.urls),
     path('account/', include('account.urls'))
 ]
